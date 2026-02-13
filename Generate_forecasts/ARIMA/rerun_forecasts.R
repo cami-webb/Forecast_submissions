926d15fd1c0cdbb496e1fe51b3aa207f59954002
Cami Webb <cwebb16@bu.edu>
2026-02-13 13:16:28 -0500

    Update rerun_forecasts.R

--- Generate_forecasts/ARIMA/rerun_forecasts.R
+++ Generate_forecasts/ARIMA/rerun_forecasts.R
@@ -1,6 +1,17 @@
 source("./Generate_forecasts/ARIMA/forecast_model.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
 
-END <- as_date('2023-11-13') #Re-run if forecasts have not been re-run after Nov 13, fixing calibration issue
+END <- as_date("2023-11-13") # Re-run if forecasts have not been re-run after Nov 13, fixing calibration issue
 
-rerun_forecasts(model_id, forecast_model, model_themes, END, noaa = F)
+# Optional knobs for different themes (coastal/urban/etc)
+# Leave NULL to use defaults inside generate_tg_forecast()
+target_path <- NULL
+
+rerun_forecasts(
+  model_id = model_id,
+  forecast_model = forecast_model,
+  model_themes = model_themes,
+  END = END,
+  noaa = FALSE,
+  all_sites = FALSE
+)
