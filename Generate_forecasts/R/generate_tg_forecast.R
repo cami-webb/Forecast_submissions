e28c56057b43ec401d61a78bfb0ea9a7907ca6fe
Cami Webb <cwebb16@bu.edu>
2026-02-13 11:18:29 -0500

    Update generate_tg_forecast.R
    Update for coastal forecasts

--- Generate_forecasts/R/generate_tg_forecast.R
+++ Generate_forecasts/R/generate_tg_forecast.R
@@ -1,124 +1,101 @@
 generate_tg_forecast <- function(forecast_date,
                                  forecast_model,
-                                 model_themes = model_themes,
+                                 model_themes = "coastal",
                                  model_id = model_id,
-                                 all_sites = F,
-                                 noaa = T,
-                                 vars_manual = NULL) {
-  #### Step 1: Define model_themes and types
-  model_types = model_themes
-  #Replace terrestrial daily and 30min with terrestrial
-  model_types[model_types %in% c("terrestrial_daily","terrestrial_30min")] <- "terrestrial" 
-  
-  #### Step 2: Get NOAA driver data
-  if(noaa){ #Some forecasts do not use any noaa driver data--> in that case skip download
-    forecast_date <- as.Date(forecast_date)
-    load_met(forecast_date) #This function loads meteorology if and only if it does not already exist
-    noaa_future_daily <- read.csv(paste0("./Generate_forecasts/noaa_downloads/noaa_future_daily_",forecast_date,".csv")) |> 
-      mutate(datetime = lubridate::as_date(datetime))
-    
-    # Load stage3 data. 
-    noaa_past_mean <- read.csv(paste0("./Generate_forecasts/noaa_downloads/noaa_past_mean_",forecast_date,".csv")) |> 
-      mutate(datetime = lubridate::as_date(datetime))
+                                 all_sites = FALSE,
+                                 noaa = FALSE,
+                                 vars_manual = NULL,
+                                 target_path = NULL,
+                                 source_mode = c("buoy_only", "modis_only")) {
+
+  forecast_date <- as.Date(forecast_date)
+
+  # NOAA drivers (not used for ARIMA; keeping in case I add them later!) 
+  if (isTRUE(noaa)) {
+    load_met(forecast_date)
+    noaa_future_daily <- read.csv(paste0("./Generate_forecasts/noaa_downloads/noaa_future_daily_", forecast_date, ".csv")) |>
+      dplyr::mutate(datetime = lubridate::as_date(datetime))
+    noaa_past_mean <- read.csv(paste0("./Generate_forecasts/noaa_downloads/noaa_past_mean_", forecast_date, ".csv")) |>
+      dplyr::mutate(datetime = lubridate::as_date(datetime))
   } else {
-    forecast_date <- as.Date(forecast_date)
     noaa_future_daily <- NULL
     noaa_past_mean <- NULL
   }
-  
-  ### AND HERE WE GO! We're ready to start forecasting ### 
-  for (theme in model_themes) {
-    if(!theme%in%c("beetles","ticks") | wday(forecast_date, label=TRUE)=="Sun"){ #beetles and ticks only want forecasts every Sunday
-      #Step 1: Download latest target data and site description data
-      target = download_target(theme)
-      type = ifelse(theme%in% c("terrestrial_30min", "terrestrial_daily"),"terrestrial",theme)
-      
-      site_data <- readr::read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv") %>%
-        filter(get(type)==1)
-      sites = site_data$field_site_id
-      
-      #Set target variables
-      if(type == "aquatics")           {vars = c("temperature","oxygen","chla")
-      horiz = 30
-      step = 1
-      }
-      if(type == "ticks")              {vars = c("amblyomma_americanum")
-      horiz = 52 #52 weeks
-      step = 7
-      }
-      if(type == "phenology")          {vars = c("gcc_90","rcc_90")
-      horiz = 30 
-      step = 1}
-      if(type == "beetles")            {vars = c("abundance","richness")
-      horiz = 52 #52 weeks
-      step = 7}
-      if(theme == "terrestrial_daily")  {vars = c("nee","le")
-      horiz = 30
-      step = 1}
-      if(theme == "terrestrial_30min")  {vars = c("nee","le")
-      horiz = 30
-      step = 1/24/2}
-      
-      if(!is.null(vars_manual)){
-        vars = vars_manual
-      }
-      
-      ## Test with a single site first!
-      #forecast <- map_dfr(vars,run_all_vars,sites[23],forecast_model,noaa_past_mean,noaa_future_daily)
-      
-      #Visualize the ensemble predictions -- what do you think?
-      #forecast |> 
-      #  ggplot(aes(x = datetime, y = prediction, group = parameter)) +
-      #  geom_line(alpha=0.3) +
-      #  facet_wrap(~variable, scales = "free")
-      
-      # Run all sites -- may be slow!
-      if(all_sites == F) {
-        forecast <- map_dfr(vars,
-                            run_all_vars,
-                            sites = sites,
-                            forecast_model = forecast_model,
-                            noaa_past_mean = noaa_past_mean,
-                            noaa_future_daily = noaa_future_daily,
-                            target = target,
-                            horiz = horiz,
-                            step = step,
-                            theme = theme,
-                            forecast_date = forecast_date)
-      } else {
-        forecast <- map_dfr(vars,
-                            forecast_model,
-                            sites = sites,
-                            noaa_past_mean = noaa_past_mean,
-                            noaa_future_daily = noaa_future_daily,
-                            target = target,
-                            horiz = horiz,
-                            step = step,
-                            theme = theme,
-                            forecast_date = forecast_date)
-      }
-      
-      if(theme %in% c("beetles","ticks")){
-        forecast = forecast%>% 
-          filter(wday(datetime, label=TRUE)=="Mon") %>% #The beetles and ticks challenges only want weekly forecasts
-          mutate(duration = "P1W")
-      } else {
-        forecast <- forecast %>%
-          mutate(duration = "P1D")
-      }
-      
-      #Forecast output file name in standards requires for Challenge.
-      # csv.gz means that it will be compressed
-      file_date <- forecast_date
-      forecast_file <- paste0(theme,"-",file_date,"-",model_id,".csv.gz")
-      
-      #Write csv to disk
-      write_csv(forecast %>%
-                  mutate(project_id = "neon4cast"), 
-                forecast_file)
-      
-      # Step 5: Submit forecast!
-      neon4cast::submit(forecast_file = forecast_file, metadata = NULL, ask = FALSE)
-    }
+
+  # Read coastal targets
+  if (is.null(target_path)) {
+    target_path <- "challenges/targets/project_id=bu4cast/coastal-targets.csv"
   }
+
+  s3 <- arrow::s3_bucket(
+    "bu4cast-ci-read",
+    endpoint_override = "https://minio-s3.apps.shift.nerc.mghpcc.org",
+    access_key = Sys.getenv("OSN_KEY"),
+    secret_key = Sys.getenv("OSN_SECRET"),
+    scheme = "https"
+  )
+
+  target <- arrow::read_csv_arrow(s3$path(target_path)) |>
+    dplyr::mutate(datetime = lubridate::as_date(substr(datetime, 1, 10))) |>
+    dplyr::select(datetime, site_id, variable, observation)
+
+  # Target variables
+  vars <- if (is.null(vars_manual)) "chlorophyll" else vars_manual
+
+  # Forecast horizon (daily)
+  horiz <- 30
+  step <- 1
+
+  # Assign site_ids as buoy vs modis
+  buoy_sites  <- unique(target$site_id[grepl("^UNH_buoy_", target$site_id)])
+  modis_sites <- unique(target$site_id[grepl("^MODIS_", target$site_id)])
+
+  theme <- "coastal"
+
+  # Loop over requested modes (default: both)
+  for (mode in source_mode) {
+
+    sites <- if (mode == "buoy_only") buoy_sites else modis_sites
+
+    if (length(sites) == 0) {
+      message("No sites found for ", mode, "; skipping.")
+      next
+    }
+
+    forecast <- purrr::map_dfr(
+      vars,
+      run_all_vars,
+      sites = sites,
+      forecast_model = forecast_model,
+      noaa_past_mean = noaa_past_mean,
+      noaa_future_daily = noaa_future_daily,
+      target = target,
+      horiz = horiz,
+      step = step,
+      theme = theme,
+      forecast_date = forecast_date
+    )
+
+    if (is.null(forecast) || nrow(forecast) == 0) {
+      message("No forecasts produced for ", mode, " (likely no usable history); skipping write/submit.")
+      next
+    }
+
+    forecast <- forecast |>
+      dplyr::mutate(duration = "P1D")
+
+    # Different filenames so buoy/modis don't overwrite each other
+    forecast_file <- paste0(theme, "-", mode, "-", forecast_date, "-", model_id, ".csv.gz")
+
+    readr::write_csv(
+      forecast |> dplyr::mutate(project_id = "bu4cast"),
+      forecast_file
+    )
+
+    neon4cast::submit(forecast_file = forecast_file, metadata = NULL, ask = FALSE)
+
+    message("Wrote + submitted: ", forecast_file)
+  }
+
+  invisible(NULL)
 }
