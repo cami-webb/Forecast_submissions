cd2c50477defcb066ca0a0bae13fbf1c3aba43f4
Cami Webb <cwebb16@bu.edu>
2026-02-24 18:23:12 -0500

    Create health_check.R

---
+++ Generate_forecasts/ARIMA/health_check.R
@@ -0,0 +1,7 @@
+message("Pinging health check...")
+tryCatch(
+  RCurl::getURL("https://hc-ping.com/1088c2cf-3fb1-43da-9ff5-d2574bdac32b"),
+  error = function(e) message("Health check ping failed: ", e$message)
+)
+
+message("Coastal targets script complete!")
