d5bc822563e25810c7bbae60fdc03d070d888524
Cami Webb <cwebb16@bu.edu>
2026-02-24 12:27:09 -0500

    Update rerun_forecasts.R

--- Generate_forecasts/ARIMA/rerun_forecasts.R
+++ Generate_forecasts/ARIMA/rerun_forecasts.R
@@ -3,8 +3,6 @@ library(lubridate)
 source("./Generate_forecasts/ARIMA/forecast_model.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
 
-END <- lubridate::as_date("2006-01-01")
-
 # Optional knobs for different themes (coastal/urban/etc)
 # Leave NULL to use defaults inside generate_tg_forecast()
 target_path <- NULL
