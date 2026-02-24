2dc377aadb63aae33f094ec78ab4500b4c87ce46
Cami Webb <cwebb16@bu.edu>
2026-02-24 13:07:36 -0500

    Update rerun_forecasts.R

--- Generate_forecasts/ARIMA/rerun_forecasts.R
+++ Generate_forecasts/ARIMA/rerun_forecasts.R
@@ -1,11 +1,15 @@
 library(lubridate)
 
 source("./Generate_forecasts/ARIMA/forecast_model.R")
+source("./Generate_forecasts/R/generate_tg_forecast.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
 
 # Optional knobs for different themes (coastal/urban/etc)
 # Leave NULL to use defaults inside generate_tg_forecast()
 target_path <- NULL
+
+model_id <- Sys.getenv("MODEL_ID", unset = "tg_arima")
+model_themes <- strsplit(Sys.getenv("MODEL_THEMES", unset = "coastal"), ",")[[1]]
 
 rerun_forecasts(
   model_id = model_id,
