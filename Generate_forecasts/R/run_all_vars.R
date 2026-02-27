8b036947c31adf19bcdc5a75f46cfbfbc0a2e496
Cami Webb <cwebb16@bu.edu>
2026-02-27 14:13:03 -0500

    Update run_all_vars.R

--- Generate_forecasts/R/run_all_vars.R
+++ Generate_forecasts/R/run_all_vars.R
@@ -11,17 +11,27 @@ run_all_vars = function(var,
                         forecast_date,
                         model_id) {
   
-  message(paste0("Running variable: ", var))
-  purrr::map_dfr(sites,
-                      forecast_model,
-                      noaa_past_mean = noaa_past_mean,
-                      noaa_future_daily = noaa_future_daily,
-                      target_variable = var,
-                      target = target,
-                      horiz = horiz,
-                      step = step,
-                      theme = theme,
-                      forecast_date = forecast_date,
-                      model_id = model_id
-                     )  
+message(
+  paste0(
+    "Running theme=", theme,
+    " var=\"", var, "\"",
+    " horiz=", horiz,
+    " step=", step
+  )
+)
+
+  purrr::map_dfr(
+  sites,
+  forecast_model,
+  noaa_past_mean = noaa_past_mean,
+  noaa_future_daily = noaa_future_daily,
+  target_variable = var,
+  target = target,
+  horiz = horiz,
+  step = step,
+  theme = theme,
+  forecast_date = forecast_date,
+  model_id = model_id
+) |>
+  dplyr::mutate(variable = var)
 }
