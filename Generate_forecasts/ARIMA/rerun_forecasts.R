efe945ed8e93a491985549b62bc823b1c45aac44
Cami Webb <cwebb16@bu.edu>
2026-02-27 14:27:17 -0500

    Update rerun_forecasts.R

--- Generate_forecasts/ARIMA/rerun_forecasts.R
+++ Generate_forecasts/ARIMA/rerun_forecasts.R
@@ -11,14 +11,25 @@ source("./Generate_forecasts/R/run_all_vars.R")
 target_path <- NULL
 
 model_id <- Sys.getenv("MODEL_ID", unset = "tg_arima")
-model_themes <- strsplit(Sys.getenv("MODEL_THEMES", unset = "coastal"), ",")[[1]]
+model_themes <- strsplit(Sys.getenv("MODEL_THEMES", unset = "coastal,urban"), ",")[[1]]
+model_themes <- trimws(model_themes)
 
-rerun_forecasts(
-  model_id = model_id,
-  forecast_model = forecast_model,
-  model_themes = model_themes,
-  END = END,
-  noaa = FALSE,
-  all_sites = FALSE,
-  source_modes = c("buoy", "modis", "cci")
-)
+for (th in model_themes) {
+
+  source_modes_th <- if (identical(th, "coastal")) {
+    c("buoy", "modis", "cci")
+  } else {
+    # urban: no buoy/modis/cci split
+    "urban"
+  }
+
+  rerun_forecasts(
+    model_id = model_id,
+    forecast_model = forecast_model,
+    model_themes = th,
+    END = END,
+    noaa = FALSE,
+    all_sites = FALSE,
+    source_modes = source_modes_th
+  )
+}
