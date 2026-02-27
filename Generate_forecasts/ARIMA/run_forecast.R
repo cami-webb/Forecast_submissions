f0380237c99308e31c1616acad7441612154b199
Cami Webb <cwebb16@bu.edu>
2026-02-27 14:36:44 -0500

    Update run_forecast.R

--- Generate_forecasts/ARIMA/run_forecast.R
+++ Generate_forecasts/ARIMA/run_forecast.R
@@ -1,14 +1,24 @@
 model_id <- "tg_arima"
-model_themes <- c("coastal")
+model_themes <- c("coastal", "urban")
 
 source("./Generate_forecasts/ARIMA/forecast_model.R")
 source("Generate_forecasts/R/generate_tg_forecast.R")
 source("Generate_forecasts/R/run_all_vars.R")
 
 tryCatch({
-  generate_tg_forecast(forecast_date = Sys.Date(),
-                       forecast_model = forecast_model,
-                       model_themes = model_themes,
-                       model_id = model_id,
-                       noaa = FALSE)
+    generate_tg_forecast(
+    forecast_date = Sys.Date(),
+    forecast_model = forecast_model,
+    model_themes = "coastal",
+    model_id = model_id,
+    noaa = FALSE
+  )
+  
+  generate_tg_forecast(
+    forecast_date = Sys.Date(),
+    forecast_model = forecast_model,
+    model_themes = "urban",
+    model_id = model_id,
+    noaa = FALSE
+  )
 }, error=function(e){cat("ERROR with forecast generation:\n",conditionMessage(e), "\n")})
