bcdcbf60355a71d2bc257df062b27f90d1e930ef
Cami Webb <cwebb16@bu.edu>
2026-03-06 12:58:33 -0500

    Update rerun_forecasts.R

--- Generate_forecasts/ARIMA/rerun_forecasts.R
+++ Generate_forecasts/ARIMA/rerun_forecasts.R
@@ -1,35 +1,24 @@
 library(lubridate)
+library(yaml)
+
+config <- yaml::read_yaml("Generate_forecasts/ARIMA/arima_config.yaml")
 
 source("./Generate_forecasts/ARIMA/forecast_model.R")
 source("./Generate_forecasts/R/generate_tg_forecast.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
 source("./Generate_forecasts/R/run_all_vars.R")
 
-
-# Optional knobs for different themes (coastal/urban/etc)
-# Leave NULL to use defaults inside generate_tg_forecast()
 target_path <- NULL
 
-model_id <- Sys.getenv("MODEL_ID", unset = "tg_arima")
-model_themes <- strsplit(Sys.getenv("MODEL_THEMES", unset = "coastal,urban"), ",")[[1]]
-model_themes <- trimws(model_themes)
-
-for (th in model_themes) {
-
-  source_modes_th <- if (identical(th, "coastal")) {
-    c("buoy", "modis", "cci")
-  } else {
-    # urban: no buoy/modis/cci split
-    "urban"
-  }
-
+for (th in config$model_themes) {
+  source_modes_th <- config$source_modes[[th]]
   rerun_forecasts(
-    model_id = model_id,
+    model_id       = config$model_id,
     forecast_model = forecast_model,
-    model_themes = th,
-    END = END,
-    noaa = FALSE,
-    all_sites = FALSE,
-    source_modes = source_modes_th
+    model_themes   = th,
+    END            = END,
+    noaa           = FALSE,
+    all_sites      = FALSE,
+    source_modes   = source_modes_th
   )
 }
