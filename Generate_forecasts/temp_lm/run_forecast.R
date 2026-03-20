9e057edacabf12ef382a24af13b5d900c5de53ad
Cami Webb <cwebb16@bu.edu>
2026-03-20 12:58:10 -0400

    Update run_forecast.R

--- Generate_forecasts/temp_lm/run_forecast.R
+++ Generate_forecasts/temp_lm/run_forecast.R
@@ -1,8 +1,25 @@
-source("./Generate_forecasts/temp_lm/forecast_model.R")
+library(lubridate)
+library(yaml)
 
-tryCatch({
-  generate_tg_forecast(forecast_date = Sys.Date(),
-                       forecast_model = forecast_model,
-                       model_themes = model_themes,
-                       model_id = model_id)
-}, error=function(e){cat("ERROR with forecast generation:\n",conditionMessage(e), "\n")})
+config <- yaml::read_yaml("Generate_forecasts/temp_lm/lm_config.yaml")
+
+source("./Generate_forecasts/temp_lm/forecast_model.R")
+source("./Generate_forecasts/R/generate_tg_forecast.R")
+source("./Generate_forecasts/R/run_all_vars.R")
+
+for (th in config$model_themes) {
+  source_modes_th <- config$source_modes[[th]]
+  tryCatch({
+    generate_tg_forecast(
+      forecast_date  = Sys.Date(),
+      forecast_model = forecast_model,
+      model_themes   = th,
+      model_id       = config$model_id,
+      all_sites      = FALSE,
+      noaa           = FALSE,
+      source_mode    = source_modes_th
+    )
+  }, error = function(e) {
+    cat("ERROR with forecast generation for theme", th, ":\n", conditionMessage(e), "\n")
+  })
+}
