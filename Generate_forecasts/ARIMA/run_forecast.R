4882b0288419823e3c736a1557ceb158f4b268c1
Cami Webb <cwebb16@bu.edu>
2026-03-20 13:07:29 -0400

    Update run_forecast.R

--- Generate_forecasts/ARIMA/run_forecast.R
+++ Generate_forecasts/ARIMA/run_forecast.R
@@ -1,18 +1,18 @@
 library(yaml)
-config <- yaml::read_yaml("Generate_forecasts/ARIMA/arima_config.yaml")
-
+config <- yaml::read_yaml("Generate_forecasts/config.yaml")
 source("./Generate_forecasts/ARIMA/forecast_model.R")
 source("Generate_forecasts/R/generate_tg_forecast.R")
 source("Generate_forecasts/R/run_all_vars.R")
 
 tryCatch({
-  for (theme in config$model_themes) {
+  for (theme in config$models$tg_arima$model_themes) {
     generate_tg_forecast(
       forecast_date  = Sys.Date(),
       forecast_model = forecast_model,
       model_themes   = theme,
-      model_id       = config$model_id,
-      noaa           = FALSE
+      model_id       = config$models$tg_arima$model_id,
+      noaa           = FALSE,
+      source_mode    = config$source_modes[[theme]]
     )
   }
 }, error = function(e) { cat("ERROR with forecast generation:\n", conditionMessage(e), "\n") })
