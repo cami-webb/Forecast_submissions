c2eef9153239608c69ff2bf260a01ab761f2a6fd
Cami Webb <cwebb16@bu.edu>
2026-03-30 15:43:09 -0400

    Delete Generate_forecasts/ARIMA/health_check.R

--- Generate_forecasts/ARIMA/health_check.R
+++
@@ -1,9 +0,0 @@
-library(yaml)
-config <- yaml::read_yaml("Generate_forecasts/ARIMA/arima_config.yaml")
-
-message("Pinging health check...")
-tryCatch(
-  RCurl::getURL(config$health_check_url),
-  error = function(e) message("Health check ping failed: ", e$message)
-)
-message("ARIMA forecasts complete! :)")
