f8b7d4a9f67c654aa07c230ff2b19d2ce6fb92bf
Cami Webb <cwebb16@bu.edu>
2026-03-30 15:30:54 -0400

    Update run_forecast.R

--- Generate_forecasts/tg_XGBoost/run_forecast.R
+++ Generate_forecasts/tg_XGBoost/run_forecast.R
@@ -1,8 +1,20 @@
+library(yaml)
+library(lubridate)
+
+config <- yaml::read_yaml("Generate_forecasts/config.yaml")
+
 source("./Generate_forecasts/tg_XGBoost/forecast_model.R")
+source("./Generate_forecasts/R/generate_tg_forecast.R")
+source("./Generate_forecasts/R/run_all_vars.R")
 
 tryCatch({
-  generate_tg_forecast(forecast_date = Sys.Date(),
-                       forecast_model = forecast_model,
-                       model_themes = model_themes,
-                       model_id = model_id)
-}, error=function(e){cat("ERROR with forecast generation:\n",conditionMessage(e), "\n")})
+  for (th in config$models$tg_xgboost$model_themes) {
+    generate_tg_forecast(
+      forecast_date  = Sys.Date(),
+      forecast_model = forecast_model,
+      model_themes   = th,
+      model_id       = config$models$tg_xgboost$model_id,
+      noaa           = TRUE
+    )
+  }
+}, error = function(e) { cat("ERROR with forecast generation:\n", conditionMessage(e), "\n") })
