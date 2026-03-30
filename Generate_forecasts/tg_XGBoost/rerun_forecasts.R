e4281ca098dafe11da32c91036595845cc3b8b94
Cami Webb <cwebb16@bu.edu>
2026-03-30 15:28:21 -0400

    Update rerun_forecasts.R

--- Generate_forecasts/tg_XGBoost/rerun_forecasts.R
+++ Generate_forecasts/tg_XGBoost/rerun_forecasts.R
@@ -1,6 +1,21 @@
+library(yaml)
+library(lubridate)
+
+config <- yaml::read_yaml("Generate_forecasts/config.yaml")
+
 source("./Generate_forecasts/tg_XGBoost/forecast_model.R")
+source("./Generate_forecasts/R/generate_tg_forecast.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
+source("./Generate_forecasts/R/run_all_vars.R")
 
-END <- as_date('2023-10-10') #Re-run if forecasts have not been re-run after Oct 10, fixing meteorology issue
-
-rerun_forecasts(model_id, forecast_model, model_themes, END, noaa = T)
+for (th in config$models$tg_xgboost$model_themes) {
+  source_modes_th <- config$source_modes[[th]]
+  rerun_forecasts(
+    model_id       = config$models$tg_xgboost$model_id,
+    forecast_model = forecast_model,
+    model_themes   = th,
+    noaa           = TRUE,
+    all_sites      = FALSE,
+    source_modes   = source_modes_th
+  )
+}
