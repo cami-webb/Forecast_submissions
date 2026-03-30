3f8c0c2f4b3ad92692e4bb6e40585a1c927615ee
Cami Webb <cwebb16@bu.edu>
2026-03-30 15:37:40 -0400

    Update rerun_forecasts.R

--- Generate_forecasts/temp_lm/rerun_forecasts.R
+++ Generate_forecasts/temp_lm/rerun_forecasts.R
@@ -1,6 +1,24 @@
+library(lubridate)
+library(yaml)
+
+config <- yaml::read_yaml("Generate_forecasts/config.yaml")
+
 source("./Generate_forecasts/temp_lm/forecast_model.R")
+source("./Generate_forecasts/R/load_met_gefs.R")
+source("./Generate_forecasts/R/generate_tg_forecast.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
+source("./Generate_forecasts/R/run_all_vars.R")
 
-END <- as_date('2023-10-10') #Re-run if forecasts have not been re-run after Oct 10, fixing meteorology issue
-
-rerun_forecasts(model_id, forecast_model, model_themes, END, noaa = T)
+for (th in config$models$tg_temp_lm$model_themes) {
+  source_modes_th <- config$source_modes[[th]]
+  rerun_forecasts(
+    model_id       = config$models$tg_temp_lm$model_id,
+    forecast_model = forecast_model,
+    model_themes   = th,
+    END            = NULL,
+    noaa           = FALSE,
+    use_gefs       = TRUE,
+    all_sites      = FALSE,
+    source_modes   = source_modes_th
+  )
+}
