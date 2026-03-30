e22d114c51b390f6c9030071d8ee70a5de82f6fa
Cami Webb <cwebb16@bu.edu>
2026-03-30 15:36:07 -0400

    Update forecast_model.R

--- Generate_forecasts/temp_lm/forecast_model.R
+++ Generate_forecasts/temp_lm/forecast_model.R
@@ -2,24 +2,14 @@
 # written by ASL, 21 Jan 2023
 # edited 2023-09-08 to consolidate and set up framework for filling in missed dates
 
-
-#### Step 0: load packages
-
 library(tidyverse)
-library(neon4cast)
 library(lubridate)
-library(glue)
-source("ignore_sigpipe.R")
-library(tsibble)
-library(fable)
 library(arrow)
-source("download_target.R")
+library(yaml)
+
 source("./Generate_forecasts/R/load_met.R")
 source("./Generate_forecasts/R/generate_tg_forecast.R")
 source("./Generate_forecasts/R/run_all_vars.R")
-
-model_themes = c("terrestrial_daily","aquatics","phenology","beetles","ticks") #By default, run model across all themes, except terrestrial 30min (not currently configured)
-model_id = "tg_temp_lm"
 
 #### Define the forecast model for a site
 forecast_model <- function(site,
@@ -32,43 +22,42 @@ forecast_model <- function(site,
                            theme,
                            forecast_date,
                            model_id) {
-  
+
   message(paste0("Running site: ", site))
 
   # Merge in past NOAA data into the targets file, matching by date.
   site_target <- target |>
     dplyr::select(datetime, site_id, variable, observation) |>
-    dplyr::filter(variable %in% c(target_variable), 
+    dplyr::filter(variable %in% c(target_variable),
                   site_id == site,
                   datetime < forecast_date) |>
     tidyr::pivot_wider(names_from = "variable", values_from = "observation") |>
-    dplyr::left_join(noaa_past_mean%>%
-                       filter(site_id == site), 
+    dplyr::left_join(noaa_past_mean %>%
+                       filter(site_id == site),
                      by = c("datetime", "site_id"))
-  
-  if(!target_variable%in%names(site_target)){
-    message(paste0("No target observations at site ",site,". Skipping forecasts at this site."))
+
+  if (!target_variable %in% names(site_target)) {
+    message(paste0("No target observations at site ", site, ". Skipping forecasts at this site."))
     return()
-    
-  } else if(sum(!is.na(site_target$air_temperature)&!is.na(site_target[target_variable]))==0){
-    message(paste0("No historical air temp data that corresponds with target observations at site ",site,". Skipping forecasts at this site."))
+
+  } else if (sum(!is.na(site_target$air_temperature) & !is.na(site_target[target_variable])) == 0) {
+    message(paste0("No historical air temp data that corresponds with target observations at site ", site, ". Skipping forecasts at this site."))
     return()
-    
+
   } else {
     # Fit linear model based on past data: target = m * air temp + b
-    fit <- lm(get(target_variable) ~ air_temperature, data = site_target) #THIS IS THE MODEL
-    
-    #  Get 30-day predicted temp ensemble at the site
-    noaa_future <- noaa_future_daily%>%
-      filter(site_id==site)
-    
-    # use the linear model (predict.lm) to forecast target variable for each ensemble member
-    forecast <- 
-      noaa_future |> 
+    fit <- lm(get(target_variable) ~ air_temperature, data = site_target)
+
+    # Get 30-day predicted temp ensemble at the site
+    noaa_future <- noaa_future_daily %>%
+      filter(site_id == site)
+
+    # use the linear model to forecast target variable for each ensemble member
+    forecast <- noaa_future |>
       mutate(site_id = site,
-             prediction = predict(fit, tibble(air_temperature)), #THIS IS THE FORECAST STEP
+             prediction = predict(fit, tibble(air_temperature)),
              variable = target_variable)
-    
+
     # Format results to EFI standard
     forecast <- forecast |>
       mutate(reference_datetime = forecast_date,
