7105305cfe8a9b73e61d8f4c8e4b2c10f4d25b34
Cami Webb <cwebb16@bu.edu>
2026-04-22 16:28:04 -0400

    Update forecast_model.R

--- Generate_forecasts/temp_lm/forecast_model.R
+++ Generate_forecasts/temp_lm/forecast_model.R
@@ -23,9 +23,18 @@ forecast_model <- function(site,
                            forecast_date,
                            model_id) {
 
-  message(paste0("Running site: ", site))
+    message(paste0("Running site: ", site))
 
-  # Merge in past NOAA data into the targets file, matching by date.
+  # Get wspd from target data
+  wspd_data <- target |>
+    dplyr::select(datetime, site_id, variable, observation) |>
+    dplyr::mutate(site_id = as.character(site_id)) |>
+    dplyr::filter(variable == "wspd",
+                  site_id == site,
+                  datetime < forecast_date) |>
+    tidyr::pivot_wider(names_from = "variable", values_from = "observation")
+
+  # Merge target variable with wspd
   site_target <- target |>
     dplyr::select(datetime, site_id, variable, observation) |>
     dplyr::mutate(site_id = as.character(site_id)) |>
@@ -33,34 +42,52 @@ forecast_model <- function(site,
                   site_id == site,
                   datetime < forecast_date) |>
     tidyr::pivot_wider(names_from = "variable", values_from = "observation") |>
-    dplyr::left_join(noaa_past_mean %>%
-                       filter(site_id == site),
-                     by = c("datetime", "site_id"))
+    dplyr::left_join(wspd_data, by = c("datetime", "site_id"))
+
   if (!target_variable %in% names(site_target)) {
     message(paste0("No target observations at site ", site, ". Skipping forecasts at this site."))
     return()
 
-  } else if (sum(!is.na(site_target$eastward_wind) & !is.na(site_target[target_variable])) == 0) {
-    message(paste0("No historical eastward wind data that corresponds with target observations at site ", site, ". Skipping forecasts at this site."))
-    
-  } else {
-    # Fit linear model based on past data: target = m * air temp + b
-    fit <- lm(get(target_variable) ~ eastward_wind, data = site_target)
-    
-    # Get 30-day predicted temp ensemble at the site
-    noaa_future <- noaa_future_daily %>%
-      filter(site_id == site)
+  } else if (sum(!is.na(site_target$wspd) & !is.na(site_target[target_variable])) == 0) {
+    message(paste0("No historical wspd data that corresponds with target observations at site ", site, ". Skipping forecasts at this site."))
+    return()
 
-    # use the linear model to forecast target variable for each ensemble member
-    forecast <- noaa_future |>
-      mutate(site_id = site,
-             prediction = predict(fit, tibble(eastward_wind)),
-             variable = target_variable)
+  } else {
+    # Fit linear model based on past data: target = m * wspd + b
+    fit <- lm(get(target_variable) ~ wspd, data = site_target)
+
+    # Get last known wspd value for persistence forecast
+    last_wspd <- wspd_data |>
+      dplyr::filter(!is.na(wspd)) |>
+      dplyr::arrange(desc(datetime)) |>
+      dplyr::slice(1) |>
+      dplyr::pull(wspd)
+
+    if (length(last_wspd) == 0 || is.na(last_wspd)) {
+      message(paste0("No recent wspd data at site ", site, ". Skipping forecasts at this site."))
+      return()
+    }
+
+    # Create future dates using persistence (same wspd for all 30 days)
+    future_dates <- seq.Date(as.Date(forecast_date),
+                             as.Date(forecast_date) + horiz - 1,
+                             by = "day")
+
+    forecast <- tibble::tibble(
+      datetime  = future_dates,
+      site_id   = as.character(site),
+      wspd      = last_wspd,
+      parameter = 1
+    ) |>
+      dplyr::mutate(
+        prediction = predict(fit, tibble::tibble(wspd = last_wspd)),
+        variable   = target_variable
+      )
 
     # Format results to EFI standard
     forecast <- forecast |>
       mutate(reference_datetime = forecast_date,
-             family = "ensemble",
+             family   = "ensemble",
              model_id = model_id) |>
       select(model_id, datetime, reference_datetime,
              site_id, family, parameter, variable, prediction)
