43dbf20830f143179014597bad72d8a16045dbf8
Cami Webb <cwebb16@bu.edu>
2026-04-22 16:29:10 -0400

    Update rerun_forecasts.R

--- Generate_forecasts/temp_lm/rerun_forecasts.R
+++ Generate_forecasts/temp_lm/rerun_forecasts.R
@@ -9,14 +9,14 @@ source("./Generate_forecasts/R/generate_tg_forecast.R")
 source("./Generate_forecasts/R/rerun_forecasts.R")
 source("./Generate_forecasts/R/run_all_vars.R")
 
-for (th in config$models$tg_ugrd_lm$model_themes) {
+for (th in config$models$tg_wspd_lm$model_themes) {
   source_modes_th <- config$source_modes[[th]]
   rerun_forecasts(
-    model_id       = config$models$tg_ugrd_lm$model_id,
+    model_id       = config$models$tg_wspd_lm$model_id,
     forecast_model = forecast_model,
     model_themes   = th,
     noaa           = FALSE,
-    use_gefs       = TRUE,
+    use_gefs       = FALSE,
     all_sites      = FALSE,
     source_modes   = source_modes_th
   )
